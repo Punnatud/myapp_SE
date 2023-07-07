8d447eb853a7bde4e67cc5910d61e95e0db56c1c
Punnatud <punnatud.d@ku.th>
2023-07-07 15:21:41 +0700

    update HelloWorld

--- src/myapp/HelloWorld.java
+++ src/myapp/HelloWorld.java
@@ -1,6 +1,8 @@
 package myapp;
 
-public class HelloWorld {
+import java.io.Serializable;
+
+public class HelloWorld implements Serializable {
     private String randomText = "Hello";
     public static void main(String[] args) {
         System.out.println("HelloWorld");
